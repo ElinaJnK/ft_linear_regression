940805cd3d5bccbe9403cc750ca6e1bb9c6fccc9
ElinaJnK <jankovskajae@yahoo.com>
2025-10-29 23:38:56 +0100

    De-normalized my values

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -31,69 +31,76 @@ def estimate_coef(x, y):
 
     return (b_0, b_1)
 
-def performLinearRegression(csv_path):
+def readCSV(csv_path):
     # csv : pandas.core.frame.DataFrame
     df = pd.read_csv(csv_path)
     # m : max rows (because that is the data we are going for)
     m = len(df)
 
-    km = df["km"]
-    price = df["price"]
-    # min-max normalization
-    df["km"] = (df["km"] - df["km"].min()) / (df["km"].max() - df["km"].min())
-    #df["price"] = (df["price"] - df["price"].min()) / (df["price"].max() - df["price"].min())
+    x = df["km"].copy()
+    y = df["price"].copy()
+    x_min, x_max = df["km"].min(), df["km"].max()
+    y_min, y_max = df["price"].min(), df["price"].max()
+    return m, x, y, x_min, x_max, y_min, y_max
 
-    learning_rate = 0.0000001
+def performLinearRegression(m, x, y, x_min, x_max, y_min, y_max):  
+    # min-max normalization
+    x_norm = (x - x_min) / (x_max - x_min)
+    y_norm = (y - y_min) / (y_max - y_min)
+
+    learning_rate = 0.1
     # at first we calculate just with y = 0*x + 0 (ax+b) to get the maximum loss
     theta0 = theta1 = 0
-    it = 1000
+    it = 10000
     prev_loss = float('inf')
     # get a certain difference between the real value vs the computed value
-    for _ in range(it):
+    for i in range(it):
         # calculate the error at each step from the actual value
-        estimate_price = theta0 + (theta1 * df["km"])
-        error = estimate_price - df["price"]
+        estimate_price = theta0 + (theta1 * x_norm)
+        error = estimate_price - y_norm
 
         # bias (theta0) and weight (theta1) derivative
         tmp_theta0 = learning_rate * (1 / m) * error.sum()
-        tmp_theta1 = learning_rate * (1 / m) * (error * df["km"]).sum()
+        tmp_theta1 = learning_rate * (1 / m) * (error * x_norm).sum()
 
         # get the new weight and bias 
         theta0 = theta0 - tmp_theta0
         theta1 = theta1 - tmp_theta1
-        #theta0 = abs(tmp_theta0)
-        #theta1 = abs(tmp_theta1)
 
         # calculate the MSE to get the loss and see if the algorithm shoudl stop
         loss = (error ** 2).mean()
         if abs(prev_loss - loss) < 1e-6:
             break
         prev_loss = loss
-    return theta0, theta1
+        
+    # denormalization of theta0 and theta1
+
+    b1 = theta1 * (y_max - y_min) / (x_max - x_min)
+    b0 = y_min + (y_max - y_min) * (theta0 - theta1 * x_min / (x_max - x_min))
+
+    return b0, b1
 
 def makeGraph(csv_path, theta0, theta1):
-    # plotting
     df = pd.read_csv(csv_path)
     x = df["km"]
-    print("x:", x)
-    print("theta0: ", theta0, "theta1: ", theta1)
-    y = theta0 + (theta1 * x)
-    print("y:", y)
-    #what I should have
-    slope, intercept, r, p, std_err = stats.mstats.linregress(x, df["price"])
+    y = df["price"]
+    
+    # Geeks for Geeks model
+    slope, intercept, r, p, std_err = stats.mstats.linregress(x, y)
     def myfunc(x):
         return slope * x + intercept
     mymodel = list(map(myfunc, x))
-    # what is in the file
     df.plot(kind = 'scatter', x = 'km', y = 'price')
-    
-    # test
     b = estimate_coef(df["km"], df["price"])
     y_pred = b[0] + b[1]*x
     plt.plot(x, y_pred, color='purple', label='Regression Line Geeks')
     
-    plt.plot(x, y, color='red', label='Regression Line')
-    #plt.plot(x, mymodel, color='blue', label='Regression Line Real')
+    # my model  
+    x = df["km"]
+    y_pred = theta0 + (theta1 * x)
+    plt.plot(x, y_pred, color='red', label='My Regression Line') 
+ 
+    plt.legend()   
     plt.title("Price of a car for a given mileage")
     plt.xlabel("Kilometers")
     plt.ylabel("Price")
@@ -103,7 +110,8 @@ def makeGraph(csv_path, theta0, theta1):
     return
 
 mileage = int(input("Please input the mileage: "))
-theta0, theta1 = performLinearRegression("./data.csv")
+m, x, y, x_min, x_max, y_min, y_max = readCSV("./data.csv")
+theta0, theta1 = performLinearRegression(m, x, y, x_min, x_max, y_min, y_max)
 estimate_price = estimatePrice(mileage, theta0, theta1)
 makeGraph("./data.csv", theta0, theta1)
 print("estimated price", estimate_price)
